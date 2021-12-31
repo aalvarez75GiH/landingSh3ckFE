ad26760b4b8e75884bb5533e0f27daee0ede28a6
Arnoldo Alvarez <p3rolix@Users-iMac.attlocal.net>
2021-12-31 10:29:02 -0500

    40th commit - Changes: updating sideBar copy (English/Spanish)

--- src/components/sideBar/sideBar.js
+++ src/components/sideBar/sideBar.js
@@ -1,10 +1,53 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
-import { Link as LinkR } from 'react-router-dom'
-import { Link as LinkS } from 'react-scroll'
+import { BiHelpCircle, BiQuestionMark } from 'react-icons/bi'
+import { GrLanguage } from 'react-icons/gr'
+// import { Link as LinkR } from 'react-router-dom'
+// import { Link as LinkS } from 'react-scroll'
 
-const SideBar = ({ toggleSideBar, isOpen }) => {
-    return (
+const SideBar = ({ toggleSideBar, isOpen, language }) => {
+    return ( 
+        // <aside
+        // className={`${isOpen ? "sideBarContainerOpen" : "sideBarContainer" }`}>
+        //     <div 
+        //     className="sideBarIcon"
+        //     onClick={ toggleSideBar }
+        //     >
+        //         <FaTimes className="closeIcon"/>
+        //     </div>
+        //     <div className="sideBarWrapper">
+        //         <ul className="sideBarMenu">
+        //             <LinkS
+        //             onClick={ toggleSideBar }
+        //             to="about" 
+        //             className="sideBarLink" >
+        //                 ¿Quieres saber más?
+        //             </LinkS>
+        //             <LinkS 
+        //             onClick={ toggleSideBar }
+        //             to="discover" 
+        //             className="sideBarLink" >
+        //                 ¿Como funciona?
+        //             </LinkS>
+        //             <LinkS 
+        //             onClick={ toggleSideBar }
+        //             to="services" 
+        //             className="sideBarLink" >
+        //                 ¿Quiéres ser parte?
+        //             </LinkS>
+        //         </ul>
+        //         <div className="sideBtnWrap">
+        //             <LinkR
+        //             className="sideBarRoute" 
+        //             to="/signin">
+        //                 Cerrar
+        //             </LinkR>
+        //         </div>
+
+        //     </div>
+
+
+        // </aside>
         <aside
         className={`${isOpen ? "sideBarContainerOpen" : "sideBarContainer" }`}>
             <div 
@@ -14,32 +57,45 @@ const SideBar = ({ toggleSideBar, isOpen }) => {
                 <FaTimes className="closeIcon"/>
             </div>
             <div className="sideBarWrapper">
-                <ul className="sideBarMenu">
-                    <LinkS
-                    onClick={ toggleSideBar }
-                    to="about" 
-                    className="sideBarLink" >
-                        ¿Quieres saber más?
-                    </LinkS>
-                    <LinkS 
-                    onClick={ toggleSideBar }
+                <div className="sideBarMenu">
+                    <div 
+                    // onClick={ toggleSideBar }
                     to="discover" 
                     className="sideBarLink" >
-                        ¿Como funciona?
-                    </LinkS>
-                    <LinkS 
-                    onClick={ toggleSideBar }
+                        <div className="mainSideBarUserOptionsIcon">
+                            <GrLanguage/>
+                        </div>
+                        Idioma 
+                        <div className="changeLanDiv">
+                            (cambiar a español)
+                        </div>
+                    </div>
+                    <div
+                    // onClick={ toggleSideBar }
+                    to="about" 
+                    className="sideBarLink" >
+                        <div className="mainSideBarUserOptionsIcon">
+                            <BiQuestionMark/>
+                        </div>
+                        Questions & Answers
+                    </div>
+                    
+                    <div 
+                    // onClick={ toggleSideBar }
                     to="services" 
                     className="sideBarLink" >
-                        ¿Quiéres ser parte?
-                    </LinkS>
-                </ul>
+                        <div className="mainSideBarUserOptionsIcon">
+                            <BiHelpCircle/>
+                        </div>
+                        Help
+                    </div>
+                </div>
                 <div className="sideBtnWrap">
-                    <LinkR
+                    <div
                     className="sideBarRoute" 
                     to="/signin">
-                        Haz Login
-                    </LinkR>
+                        Cerrar
+                    </div>
                 </div>
 
             </div>
