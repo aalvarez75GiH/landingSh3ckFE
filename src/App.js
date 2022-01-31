88fb9c58bff11963e28083ba5f0c599c236859a8
Arnoldo Alvarez <p3rolix@Users-iMac.attlocal.net>
2022-01-31 11:45:27 -0500

    50th commit - Changes: Adding aaplications.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Routes } from 'react-router-dom'
 import Home from './views/home'
 import GoogleAuth from './components/buttons/googleAuth'
 import GoogleAuth2 from './components/buttons/googleAuth2'
-import './App.css'
+// import './App.css'
 // import './main.css'
 
 const App = () => {
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import  ReactDOM  from 'react-dom'
 import App from './App'
+import Application from './components/buttons/applications'
 
 const container = document.getElementById('root')
 
 ReactDOM.render(
 
 <React.StrictMode>
-    <App/>
+    <Application/>
 </React.StrictMode>,
 container
 
