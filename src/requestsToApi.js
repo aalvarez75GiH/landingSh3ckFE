fd45c61007732aba0e6f82992d99716063d5a299
Arnoldo Alvarez <p3rolix@Users-iMac.attlocal.net>
2022-03-22 08:25:15 -0400

    172th commit - Changes: Updating cloud

--- src/requestsToApi.js
+++ src/requestsToApi.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-const URL_LOCAL_BACKEND = 'http://192.168.1.102:5000'
-// const URL_CLOUD_BACKEND = 'https://intense-atoll-00786.herokuapp.com'
+// const URL_LOCAL_BACKEND = 'http://192.168.1.102:5000'
+const URL_CLOUD_BACKEND = 'https://intense-atoll-00786.herokuapp.com'
 
 export const verifyingTokenRequest = async(token) => {
-    return await axios.get(`${URL_LOCAL_BACKEND}/api/users/me`, {
+    return await axios.get(`${URL_CLOUD_BACKEND}/api/users/me`, {
     headers:{
         Authorization: `Bearer ${token}` 
     }})
@@ -12,7 +12,7 @@ export const verifyingTokenRequest = async(token) => {
 }
 
 export const addingGoogleUserToDB = async(token) => {
-    return await axios.post(`${URL_LOCAL_BACKEND}/api/extUsers/google`, {
+    return await axios.post(`${URL_CLOUD_BACKEND}/api/extUsers/google`, {
         token,
         headers:{
             'Content-Type': 'application/json',
@@ -21,25 +21,25 @@ export const addingGoogleUserToDB = async(token) => {
 } 
 
 export const addingInterestedUserToDB = async(interestedUser) => {
-    return await axios.post(`${URL_LOCAL_BACKEND}/api/interestedUsers`, interestedUser)
+    return await axios.post(`${URL_CLOUD_BACKEND}/api/interestedUsers`, interestedUser)
 }
 
 export const requestToLoginUsers = async(user) => {
-    return await axios.post(`${URL_LOCAL_BACKEND}/api/users/login`, user)
+    return await axios.post(`${URL_CLOUD_BACKEND}/api/users/login`, user)
 }
 
 export const addingRegularUsersToDB = async(user) => {
-    return await axios.post(`${URL_LOCAL_BACKEND}/api/users`, user)
+    return await axios.post(`${URL_CLOUD_BACKEND}/api/users`, user)
 }
 
 export const requestToGenerateNewPIN = async(dataToRequest) => {
-    return await axios.put(`${URL_LOCAL_BACKEND}/api/users/newPIN`, dataToRequest)
+    return await axios.put(`${URL_CLOUD_BACKEND}/api/users/newPIN`, dataToRequest)
 }
 
 export const getRequestToCities = async() => {
     const token = localStorage.getItem("SH3CK_TOKEN")
     console.log(token)
-    const response = await axios.get(`${URL_LOCAL_BACKEND}/api/cities`, {
+    const response = await axios.get(`${URL_CLOUD_BACKEND}/api/cities`, {
         headers:{
             'Content-Type': 'application/json',
             Authorization: `Bearer ${token}`   
@@ -51,7 +51,7 @@ export const getRequestToCities = async() => {
 export const getRequestToCategories = async() => {
     const token = localStorage.getItem("SH3CK_TOKEN")
     console.log(token)
-    const response = await axios.get(`${URL_LOCAL_BACKEND}/api/category`, {
+    const response = await axios.get(`${URL_CLOUD_BACKEND}/api/category`, {
         headers:{
             'Content-Type': 'application/json',
             Authorization: `Bearer ${token}`   
@@ -63,7 +63,7 @@ export const getRequestToCategories = async() => {
 export const getRequestToServiceTime = async() => {
     const token = localStorage.getItem("SH3CK_TOKEN")
     console.log(token)
-    const response = await axios.get(`${URL_LOCAL_BACKEND}/api/times`, {
+    const response = await axios.get(`${URL_CLOUD_BACKEND}/api/times`, {
         headers:{
             'Content-Type': 'application/json',
             Authorization: `Bearer ${token}`   
