bdc0e739d69ee318a8e0f265c3afbff012da37e8
Arnoldo Alvarez <p3rolix@Users-iMac.attlocal.net>
2022-03-27 23:06:32 -0400

    181th commit - Changes: We added material ui rating component read only at checker tiles

--- src/components/checkApp/checkAppUtilities.js
+++ src/components/checkApp/checkAppUtilities.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { Stack, Rating } from '@mui/material'
 import { Link as LinkS } from 'react-scroll'
 import  { useSelector, useDispatch }  from 'react-redux'
 import { bindActionCreators } from '@reduxjs/toolkit'
@@ -298,21 +299,25 @@ export const CheckerTile = ({ checker, index }) => {
                     
             </div>
             <div className="checkerInfoContainer">
-            <div className="checkerInfo">
+                <div className="checkerInfo">
                     <div className="checkerInfo__name">
                         <h1>{checkerNameCapitalized}</h1>
                     </div>
                     <div className="checkerInfo__ciudad">
                         <h3>{checker.city_name}</h3>
                     </div>
+                    {/* <Stack spacing={1}>
+                     <Rating value={4} readOnly/>
+                    </Stack> */}
                     <div className="checkerInfo__rating">
-                        <h3>Rating:</h3><h4>{checker.rating}</h4>
+                    <h3>Rating:</h3><Rating value={checker.rating} readOnly /> <h4>({checker.rating})</h4>
+                        {/* <h4>{checker.rating}</h4> */}
                     </div>
                     <CategoryLabelComponent checker={checker}/>
                    
                 </div>
                 <div className="checkerBioButton">
-                    <p>¿Que dicen de esta persona?</p>  
+                    <p>Ver más...</p>  
                 </div>
             </div>
         </LinkS>
